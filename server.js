487a07e3bf712199e630277e649312ae17e25ecc
GuiJg <j.gu1lh3rm325@gmail.com>
2024-09-02 09:50:46 -0300

    backend initial commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(authMiddleware);
 app.use('/api/project', projectRoute);
 
 //usuarios rota
-app.use('/api/user', userRoute);
+app.use('/api/user', userRoute);  
 
 mongoose.connect(MONGO_URL)
     .then(() => {
